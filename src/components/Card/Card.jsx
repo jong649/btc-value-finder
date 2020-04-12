8d5c43a0fbf26505c5cb23cb861be974d8c63e8c
Jon <jong649@gmail.com>
2020-04-12 15:32:23 -0700

    button click updates USD value

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,26 +4,39 @@ class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: "USD",
-      symbol: "&#36;",
-      rate: "",
-      description: "United States Dollar",
-      rate_float: 0.0,
+      USD: {
+        code: "USD",
+        symbol: "$",
+        description: "United States Dollar",
+        rate_float: 0.0,
+      },
     };
   }
-  handleClick() {
+
+  handleClick = () => {
     console.log("clicked");
     const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
     fetch(url)
       .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
+      .then((data) => {
+        console.log(data.bpi.USD.rate_float);
+        let clone = Object.assign({}, this.state);
+
+        clone.USD.rate_float = data.bpi.USD.rate_float;
+        this.setState(clone);
+        console.log(clone.USD);
+      });
+  };
+
   render() {
     return (
       <div>
         <h1>Card</h1>
         <h2>Currency</h2>
-        <div>{this.state.code}</div>
+        <div>
+          {this.state.USD.code} {this.state.USD.symbol}
+          {this.state.USD.rate_float}
+        </div>
         <button onClick={this.handleClick}>Get Value</button>
       </div>
     );
